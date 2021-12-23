a56ed9d7083ef49e9189d4e0486aa8e48da3f4e4
devpla <chaeyeonhee@kakao.com>
2021-12-23 22:13:18 +0900

    feat: Update main page and delete router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,35 +1,20 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import Chat from "../views/Chat.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-  {
-    path: "/login",
-    name: "Login",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Login.vue"),
+    name: "login",
+    component: Login,
   },
   {
     path: "/chat",
     name: "chat",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Chat.vue"),
+    component: Chat,
   },
 ];
 
