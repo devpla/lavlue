9b071640de9cccc6ff26ce86fae759a1e2ea53f4
devpla <chaeyeonhee@kakao.com>
2021-12-23 22:08:43 +0900

    feat: Add prev data and update design

--- frontend/src/components/ChatList.vue
+++ frontend/src/components/ChatList.vue
@@ -1,16 +1,34 @@
 <template>
-  <div>
-    <h4>This is chat list</h4>
-    <ul v-for="(msg, index) in msgs" :key="index">
-      <li>{{ msg.from.name }} : {{ msg.msg }}</li>
-    </ul>
+  <div class="chat__body" id="chat__body">
+    <chat-message
+      v-for="(msg, index) in msgs"
+      :key="index"
+      :msg="msg"
+      :prev="[index == 0 ? null : msgs[index - 1]]"
+    >
+    </chat-message>
   </div>
 </template>
 
 <script>
+import ChatMessage from "@/components/ChatMessage";
+
 export default {
+  components: {
+    ChatMessage,
+  },
   props: ["msgs"],
 };
 </script>
 
-<style></style>
+<style>
+.chat__body {
+  padding: 2rem;
+  overflow: scroll;
+  scroll-behavior: smooth;
+}
+
+.chat__body::-webkit-scrollbar {
+  display: none;
+}
+</style>
